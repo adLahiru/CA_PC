6e7dcdbb9ab55cda84e0914ab9832a6fe98f8823
VIHANGA <vihanga2003nimsara@gmail.com>
2025-03-12 10:58:05 +0530

    item data load done

--- PC_Lab/src/edu/cse/SuperSaverPOSGroup_Found404.java
+++ PC_Lab/src/edu/cse/SuperSaverPOSGroup_Found404.java
@@ -9,13 +9,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package edu.cse;
-
 import java.util.Scanner;
 
 /**
@@ -91,6 +84,8 @@ public class SuperSaverPOSGroup_Found404 {
      */
     public static void main(String[] args) {
 
+        DB db = new DB();
+
         // Show Welocome message of shop
         String welcome = "Welcome to Found404 Pvt Ltd";
         int display_length = 70;
@@ -135,4 +130,61 @@ public class SuperSaverPOSGroup_Found404 {
 
     }
 
+}
+
+class DB {
+    private List<Item> items = new ArrayList<>();
+
+    public void loadItemsFromCSV(String filePath) {
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+
+                SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+                Date manufacDate = new Date();
+                Date expireDate = new Date();
+
+                try {
+                    manufacDate = formatter.parse(values[4]);
+                    expireDate = formatter.parse(values[5]);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                Item item = new Item(Integer.parseInt(values[0]), values[1], Double.parseDouble(values[2]),
+                        Double.parseDouble(values[3]), manufacDate, expireDate, values[6], Integer.parseInt(values[7]));
+                items.add(item);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class Item {
+        private int itemCode;
+        private String name;
+        private double price;
+        double weight;
+        Date manDate;
+        Date expDate;
+        String manufacturer;
+        private int quantity;
+
+        public Item(int itemCode, String name, double price, double weight, Date manufacDate, Date expireDate,
+                String manufac, int quantity) {
+            this.itemCode = itemCode;
+            this.name = name;
+            this.price = price;
+            this.weight = weight;
+            this.manDate = manufacDate;
+            this.expDate = expireDate;
+            this.manufacturer = manufac;
+            this.quantity = quantity;
+        }
+    }
+
+    public DB() {
+        loadItemsFromCSV("./itemData.csv");
+    }
 }
